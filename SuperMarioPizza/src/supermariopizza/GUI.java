1dc001984a31a1ecaf25d30b75644b04d1fefc9e
MissBrain <MissBrain@users.noreply.github.com>
2015-06-02 18:40:49 +0200

    ArrayList Speisekarte mit erstem Test-Element

--- SuperMarioPizza/src/supermariopizza/GUI.java
+++ SuperMarioPizza/src/supermariopizza/GUI.java
@@ -3,12 +3,19 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
-
+import java.util.*;
 public class GUI {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		erstellungSpeisekarte();
+	}
+	
+	static void erstellungSpeisekarte(){
+		ArrayList <Bestellung> li_speisekarte = new ArrayList <Bestellung>();
+		Bestellung test = new Bestellung("test", 10);
+		li_speisekarte.add(test);
+		System.out.println(li_speisekarte.get(0).name);
 	}
 
 }

f64ed8d73da1838ce7fdb787848e62c2b60bab80
MissBrain <MissBrain@users.noreply.github.com>
2015-06-03 17:46:13 +0200

    ArrayList für Getränke + Test

--- SuperMarioPizza/src/supermariopizza/GUI.java
+++ SuperMarioPizza/src/supermariopizza/GUI.java
@@ -9,13 +9,27 @@ public class GUI {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		erstellungSpeisekarte();
+		erstellungGetraenkekarte();
 	}
 	
+	//Erstellung der ArrayList in der die Gerichte stehen
 	static void erstellungSpeisekarte(){
 		ArrayList <Bestellung> li_speisekarte = new ArrayList <Bestellung>();
-		Bestellung test = new Bestellung("test", 10);
-		li_speisekarte.add(test);
+		
+		Bestellung Test1 = new Bestellung("Test1", 10);
+		li_speisekarte.add(Test1);
 		System.out.println(li_speisekarte.get(0).name);
+		System.out.println(li_speisekarte.get(0).preis);
 	}
 
+	//Erstellung der ArrayList in der die Getränke stehen
+	static void erstellungGetraenkekarte(){
+		ArrayList <Getränke> li_getraenkekarte = new ArrayList <Getränke>();
+		
+		Getränke Test = new Getränke("Test2", 15, 0.5F);
+		li_getraenkekarte.add(Test);
+		System.out.println(li_getraenkekarte.get(0).name);
+		System.out.println(li_getraenkekarte.get(0).preis);
+		System.out.println(li_getraenkekarte.get(0).menge);
+	}
 }

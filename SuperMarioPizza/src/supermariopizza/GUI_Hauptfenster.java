94c00e67a80a0ccc16f9e427454bba3f3364fbcb
ScheichTobi <ScheichTobi@users.noreply.github.com>
2015-06-06 15:56:41 +0200

    +Musik kann nun gestoppt und neu abgespielt werden

--- SuperMarioPizza/src/supermariopizza/GUI_Hauptfenster.java
+++ SuperMarioPizza/src/supermariopizza/GUI_Hauptfenster.java
@@ -10,12 +10,14 @@ import javax.swing.JButton;
 
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.Component;
 import java.awt.GridLayout;
 
 public class GUI_Hauptfenster extends JFrame {
 
 	private JPanel contentPane;
-
+	JButton btnMusikStoppen = new JButton("Musik stoppen");
+	JButton btnMusikNeuAbspielen = new JButton("Musik neu abspielen");
 	public GUI_Hauptfenster() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
@@ -23,19 +25,24 @@ public class GUI_Hauptfenster extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		
-		JButton btnMusikStoppen = new JButton("Musik stoppen");
+		
 		btnMusikStoppen.setBounds(10, 11, 165, 46);
 		btnMusikStoppen.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				GUI.Musik.stop();
+				btnMusikStoppen.setEnabled(false);
+				btnMusikNeuAbspielen.setEnabled(true);
 			}
 		});
 		contentPane.setLayout(null);
 		contentPane.add(btnMusikStoppen);
 		
-		JButton btnMusikNeuAbspielen = new JButton("Musik neu abspielen");
+		
+		btnMusikNeuAbspielen.setEnabled(false);
 		btnMusikNeuAbspielen.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				btnMusikNeuAbspielen.setEnabled(false);
+				btnMusikStoppen.setEnabled(true);
 				try {
 					GUI.sound();
 				} catch (Exception e1) {}

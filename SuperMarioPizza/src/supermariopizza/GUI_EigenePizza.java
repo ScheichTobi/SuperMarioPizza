7da74f2f1eff0e4bd75caa65156f5669c50fc0ba
ScheichTobi <ScheichTobi@users.noreply.github.com>
2015-06-07 17:02:03 +0200

    *Schriftart geändert 2.0

--- SuperMarioPizza/src/supermariopizza/GUI_EigenePizza.java
+++ SuperMarioPizza/src/supermariopizza/GUI_EigenePizza.java
@@ -47,50 +47,59 @@ public class GUI_EigenePizza extends JFrame {
 		EigenePizzaGui.setLayout(null);
 		
 		JCheckBox chb_Zwiebeln = new JCheckBox("Zwiebeln");
-		chb_Zwiebeln.setFont(new Font("Supercell-Magic", Font.PLAIN, 11));
-		chb_Zwiebeln.setBounds(94, 119, 97, 23);
+		chb_Zwiebeln.setFont(new Font("Albertus Extra Bold", Font.PLAIN, 20));
+		chb_Zwiebeln.setBounds(94, 119, 144, 23);
 		EigenePizzaGui.add(chb_Zwiebeln);
 		
 		JCheckBox chb_Pilze = new JCheckBox("Pilze");
+		chb_Pilze.setFont(new Font("Albertus Extra Bold", Font.PLAIN, 20));
 		chb_Pilze.setBounds(94, 159, 97, 23);
 		EigenePizzaGui.add(chb_Pilze);
 		
 		JCheckBox chb_Schinken = new JCheckBox("Schinken");
-		chb_Schinken.setBounds(94, 205, 97, 23);
+		chb_Schinken.setFont(new Font("Albertus Extra Bold", Font.PLAIN, 20));
+		chb_Schinken.setBounds(94, 205, 144, 23);
 		EigenePizzaGui.add(chb_Schinken);
 		
 		JCheckBox chb_Salami = new JCheckBox("Salami");
+		chb_Salami.setFont(new Font("Albertus Extra Bold", Font.PLAIN, 20));
 		chb_Salami.setBounds(94, 251, 97, 23);
 		EigenePizzaGui.add(chb_Salami);
 		
 		JCheckBox chb_Sardellen = new JCheckBox("Sardellen");
-		chb_Sardellen.setBounds(94, 302, 97, 23);
+		chb_Sardellen.setFont(new Font("Albertus Extra Bold", Font.PLAIN, 20));
+		chb_Sardellen.setBounds(94, 302, 122, 23);
 		EigenePizzaGui.add(chb_Sardellen);
 		
 		JCheckBox chb_Paprika = new JCheckBox("Paprika");
+		chb_Paprika.setFont(new Font("Albertus Extra Bold", Font.PLAIN, 20));
 		chb_Paprika.setBounds(495, 119, 97, 23);
 		EigenePizzaGui.add(chb_Paprika);
 		
 		JCheckBox chb_Meeresfrüchte = new JCheckBox("Meeresfr\u00FCchte");
-		chb_Meeresfrüchte.setBounds(495, 159, 97, 23);
+		chb_Meeresfrüchte.setFont(new Font("Albertus Extra Bold", Font.PLAIN, 20));
+		chb_Meeresfrüchte.setBounds(495, 159, 180, 23);
 		EigenePizzaGui.add(chb_Meeresfrüchte);
 		
 		JCheckBox chb_Peperoni = new JCheckBox("Peperoni");
-		chb_Peperoni.setBounds(495, 205, 97, 23);
+		chb_Peperoni.setFont(new Font("Albertus Extra Bold", Font.PLAIN, 20));
+		chb_Peperoni.setBounds(495, 205, 161, 23);
 		EigenePizzaGui.add(chb_Peperoni);
 		
 		JCheckBox chb_Ananas = new JCheckBox("Ananas");
+		chb_Ananas.setFont(new Font("Albertus Extra Bold", Font.PLAIN, 20));
 		chb_Ananas.setBounds(495, 251, 97, 23);
 		EigenePizzaGui.add(chb_Ananas);
 		
 		JCheckBox chb_Rucola = new JCheckBox("Rucola");
+		chb_Rucola.setFont(new Font("Albertus Extra Bold", Font.PLAIN, 20));
 		chb_Rucola.setBounds(495, 302, 97, 23);
 		EigenePizzaGui.add(chb_Rucola);
 		
 		JLabel lblGrundprei = new JLabel("Grundprei\u00DF  5,-\u20AC + 0,30 \u20AC je Zutat");
-		lblGrundprei.setFont(new Font("Supercell-Magic", Font.PLAIN, 11));
+		lblGrundprei.setFont(new Font("Albertus Extra Bold", Font.PLAIN, 20));
 		lblGrundprei.setForeground(Color.RED);
-		lblGrundprei.setBounds(284, 39, 267, 23);
+		lblGrundprei.setBounds(152, 39, 360, 23);
 		EigenePizzaGui.add(lblGrundprei);
 		
 		textField = new JTextField();

cacd36b9d99c899c6cb1948f333414c544fcadb2
IPoLDeY <poldistotzem95@web.de>
2015-06-07 16:27:22 +0200

    Uhlala

--- SuperMarioPizza/src/supermariopizza/EigenePizzaGUI.java
+++ SuperMarioPizza/src/supermariopizza/EigenePizzaGUI.java
@@ -6,10 +6,16 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JCheckBox;
+import javax.swing.JLabel;
+import java.awt.Color;
+import javax.swing.JButton;
 
 public class EigenePizzaGUI extends JFrame {
 
-	private JPanel contentPane;
+	private JPanel EigenePizzaGui;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -32,11 +38,72 @@ public class EigenePizzaGUI extends JFrame {
 	 */
 	public EigenePizzaGUI() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
+		setBounds(500, 500, 850, 538);
+		EigenePizzaGui = new JPanel();
+		EigenePizzaGui.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(EigenePizzaGui);
+		EigenePizzaGui.setLayout(null);
+		
+		JCheckBox Zutat1 = new JCheckBox("New check box");
+		Zutat1.setBounds(94, 119, 97, 23);
+		EigenePizzaGui.add(Zutat1);
+		
+		JCheckBox Zutat2 = new JCheckBox("New check box");
+		Zutat2.setBounds(94, 159, 97, 23);
+		EigenePizzaGui.add(Zutat2);
+		
+		JCheckBox Zutat3 = new JCheckBox("New check box");
+		Zutat3.setBounds(94, 205, 97, 23);
+		EigenePizzaGui.add(Zutat3);
+		
+		JCheckBox Zutat4 = new JCheckBox("New check box");
+		Zutat4.setBounds(94, 251, 97, 23);
+		EigenePizzaGui.add(Zutat4);
+		
+		JCheckBox Zutat5 = new JCheckBox("New check box");
+		Zutat5.setBounds(94, 302, 97, 23);
+		EigenePizzaGui.add(Zutat5);
+		
+		JCheckBox Zutat6 = new JCheckBox("New check box");
+		Zutat6.setBounds(495, 119, 97, 23);
+		EigenePizzaGui.add(Zutat6);
+		
+		JCheckBox Zutat7 = new JCheckBox("New check box");
+		Zutat7.setBounds(495, 159, 97, 23);
+		EigenePizzaGui.add(Zutat7);
+		
+		JCheckBox Zutat8 = new JCheckBox("New check box");
+		Zutat8.setBounds(495, 205, 97, 23);
+		EigenePizzaGui.add(Zutat8);
+		
+		JCheckBox Zutat9 = new JCheckBox("New check box");
+		Zutat9.setBounds(495, 251, 97, 23);
+		EigenePizzaGui.add(Zutat9);
+		
+		JCheckBox Zutat10 = new JCheckBox("New check box");
+		Zutat10.setBounds(495, 302, 97, 23);
+		EigenePizzaGui.add(Zutat10);
+		
+		JLabel lblGrundprei = new JLabel("Grundprei\u00DF  5,-\u20AC + 0,30 \u20AC je Zutat");
+		lblGrundprei.setForeground(Color.RED);
+		lblGrundprei.setBounds(284, 39, 180, 23);
+		EigenePizzaGui.add(lblGrundprei);
+		
+		textField = new JTextField();
+		textField.setBounds(52, 376, 214, 51);
+		EigenePizzaGui.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNameFrIhre = new JLabel("Name f\u00FCr ihre Pizza eingeben:");
+		lblNameFrIhre.setBounds(52, 343, 214, 23);
+		EigenePizzaGui.add(lblNameFrIhre);
+		
+		JLabel lblAktuellerPrei = new JLabel("Aktueller Prei\u00DF");
+		lblAktuellerPrei.setBounds(387, 376, 180, 23);
+		EigenePizzaGui.add(lblAktuellerPrei);
+		
+		JButton btnWeiter = new JButton("Weiter");
+		btnWeiter.setBounds(586, 415, 224, 56);
+		EigenePizzaGui.add(btnWeiter);
 	}
-
 }

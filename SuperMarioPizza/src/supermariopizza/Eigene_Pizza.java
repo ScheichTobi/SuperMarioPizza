cae619c23f0c9e66ebd5ceec5cb1bec2306b4092
ScheichTobi <ScheichTobi@users.noreply.github.com>
2015-06-08 18:31:16 +0200

    +Viele Kommentare zu Eigene_Pizza hinzugefügt

--- SuperMarioPizza/src/supermariopizza/Eigene_Pizza.java
+++ SuperMarioPizza/src/supermariopizza/Eigene_Pizza.java
@@ -1,17 +1,16 @@
 package supermariopizza;
 
-
-
 import java.text.DecimalFormat;
 
 
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
-
 public class Eigene_Pizza extends Pizza{
+	
 	private static int zutatenmenge;
-	static int staticanzahl = 1;
+	static int pizzaanzahl = 1;
+	
 	//Konstruktor
 	public Eigene_Pizza(String name, String preis, int anzahl, String[] zutaten) {
 		super(name, preis, anzahl);
@@ -20,6 +19,7 @@ public class Eigene_Pizza extends Pizza{
 	//Zutaten ermitteln
 	public static String[] ZutatenErmitteln(){
 		
+		//Überprüfen wie viele Zutaten ausgewählt wurden
 		zutatenmenge = 0;
 		if(GUI_EigenePizza.chb_ananas.isSelected()== true)zutatenmenge ++;
 		if(GUI_EigenePizza.chb_zwiebeln.isSelected()== true)zutatenmenge ++;
@@ -31,7 +31,8 @@ public class Eigene_Pizza extends Pizza{
 		if(GUI_EigenePizza.chb_rucola.isSelected()== true)zutatenmenge ++;
 		if(GUI_EigenePizza.chb_meeresfruechte.isSelected()== true)zutatenmenge ++;
 		if(GUI_EigenePizza.chb_paprika.isSelected()== true)zutatenmenge ++;
-			
+		
+		//Erstellung von Zutaten-Strings, die jeder eigenen Pizza mitgegeben werden
 		String ananas = "ananas";
 		String zwiebeln = "zwiebeln";
 		String pilze = "pilze";
@@ -43,12 +44,10 @@ public class Eigene_Pizza extends Pizza{
 		String meeresfruechte = "meeresfruechte";
 		String paprika = "paprika";
 		
-		
-		
-		
-		
+		//Erstellung eines String-Arrays mit der Größe der ausgewählten Zutaten
 		String[] zutaten = new String [zutatenmenge];
 		
+		//Wenn Zutat ausgewählt, füge sie in das String-Array ein
 		for (int i = 0; i < zutaten.length; i++) {
 			if(GUI_EigenePizza.chb_ananas.isSelected()== true){
 				zutaten[i] = ananas;
@@ -90,8 +89,6 @@ public class Eigene_Pizza extends Pizza{
 				zutaten[i] = paprika;
 				i++;
 			}
-			
-			
 		}
 		return zutaten;
 	}
@@ -99,48 +96,65 @@ public class Eigene_Pizza extends Pizza{
 	//Anzahl der eigenen Pizza ermitteln
 	public static int AnzahlErmitteln(){
 		int anzahl = 0;
+		//Zahl im JSpinner ermitteln
 		anzahl = (int) GUI_EigenePizza.sp_pizzamenge.getValue();
-		staticanzahl = anzahl;
+		pizzaanzahl = anzahl;
 		return anzahl;
 	}
 	
 	//Name der eigenen Pizza ermitteln
 	public static String NameErmitteln(){
+		//String im JTextField ermitteln
 		String name = null;
 		name = GUI_EigenePizza.txt_pizzaname.getText();
 		return name;
 	}
 	//Preis der eigenen Pizze ermitteln
 	public static String PreisErmitteln(){
+		//Grundpreis
 		double preis = 5.0;
-		if(GUI_EigenePizza.chb_ananas.isSelected()== true)preis += 0.5;
-		if(GUI_EigenePizza.chb_zwiebeln.isSelected()== true)preis += 0.5;
-		if(GUI_EigenePizza.chb_pilze.isSelected()== true)preis += 0.5;
-		if(GUI_EigenePizza.chb_schinken.isSelected()== true)preis += 0.5;
-		if(GUI_EigenePizza.chb_salami.isSelected()== true)preis += 0.5;
-		if(GUI_EigenePizza.chb_sardellen.isSelected()== true)preis += 0.5;
-		if(GUI_EigenePizza.chb_peperoni.isSelected()== true)preis += 0.5;
-		if(GUI_EigenePizza.chb_rucola.isSelected()== true)preis += 0.5;
-		if(GUI_EigenePizza.chb_meeresfruechte.isSelected()== true)preis += 0.5;
-		if(GUI_EigenePizza.chb_paprika.isSelected()== true)preis += 0.5;
-		preis *= staticanzahl;
-		staticanzahl = 1;
+		
+		//Preis pro Zutat
+		double zutatenpreis = 0.5;
+		
+		//Falls Zutat ausgewählt, Preis um Zutatenpreis erhöhen
+		if(GUI_EigenePizza.chb_ananas.isSelected()== true)preis += zutatenpreis;
+		if(GUI_EigenePizza.chb_zwiebeln.isSelected()== true)preis += zutatenpreis;
+		if(GUI_EigenePizza.chb_pilze.isSelected()== true)preis += zutatenpreis;
+		if(GUI_EigenePizza.chb_schinken.isSelected()== true)preis += zutatenpreis;
+		if(GUI_EigenePizza.chb_salami.isSelected()== true)preis += zutatenpreis;
+		if(GUI_EigenePizza.chb_sardellen.isSelected()== true)preis += zutatenpreis;
+		if(GUI_EigenePizza.chb_peperoni.isSelected()== true)preis += zutatenpreis;
+		if(GUI_EigenePizza.chb_rucola.isSelected()== true)preis += zutatenpreis;
+		if(GUI_EigenePizza.chb_meeresfruechte.isSelected()== true)preis += zutatenpreis;
+		if(GUI_EigenePizza.chb_paprika.isSelected()== true)preis += zutatenpreis;
+		
+		//Mit ausgewählter Anzahl multiplizieren
+		preis *= pizzaanzahl;
+		
+		//Pizzaanzahl zurücksetzen
+		pizzaanzahl = 1;
+		
+		//Preis von Double in String verwandeln
 		String preis2 = String.valueOf(preis);
 		preis2 = format(preis);
 		
+		//Aktuellen Preis in GUI_EigenePizza anzeigen lassen
 		GUI_EigenePizza.lbl_pizzapreisanzeige.setText(preis2 + "€");
-		
 		
 		return preis2;
 	}
+	
+	//Wandeln einen double Wert z.B 6.5 in 6.50 um
 	public static String format(double i)
 	{
 		DecimalFormat f = new DecimalFormat("#0.00");
 		double toFormat = ((double)Math.round(i*100))/100;
 		return f.format(toFormat);
 	}
+	
+	//Fehlerroutine
 	public static Boolean CheckForErrors(){
-		//Fehlerüberprüfung
 		
 		//JSpinner kleiner als 1
 		if((int)GUI_EigenePizza.sp_pizzamenge.getValue() < 1){

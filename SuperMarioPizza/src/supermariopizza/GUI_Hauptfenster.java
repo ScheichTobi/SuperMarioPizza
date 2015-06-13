c52288e40d9c98f73bd166b1aede5e9687478525
IPoLDeY <poldistotzem95@web.de>
2015-06-13 18:15:27 +0200

    Kuckmal

--- SuperMarioPizza/src/supermariopizza/GUI_Hauptfenster.java
+++ SuperMarioPizza/src/supermariopizza/GUI_Hauptfenster.java
@@ -303,7 +303,10 @@ public class GUI_Hauptfenster extends JFrame {
 		DefaultTableModel model = (DefaultTableModel) table.getModel();
 		model.addRow(new Object[]{GUI.li_speisekarte.get(auswahl).name, anzahl, Eigene_Pizza.format(Double.parseDouble((GUI.li_speisekarte.get(auswahl).preis)) * anzahl) + "€"});
 		GUI.hinzufuegensound();
+		
 		GUI.li_Rechnung.add(GUI.li_speisekarte.get(auswahl));
+		
+		
 		for(int i=0;i<GUI.li_Rechnung.size();i++){
 			System.out.println(GUI.li_Rechnung.get(i).name);
 		}
@@ -321,9 +324,10 @@ public class GUI_Hauptfenster extends JFrame {
 			return;
 		}
 		GUI_Hauptfenster.table.setSize((int)GUI_Hauptfenster.table.getBounds().getWidth(), (int)GUI_Hauptfenster.table.getBounds().getHeight() + GUI_Hauptfenster.table.getRowHeight());
-		
+		GUI.li_Rechnung.add(GUI.li_getraenkekarte.get(auswahl));
 		DefaultTableModel model = (DefaultTableModel) table.getModel();
 		model.addRow(new Object[]{GUI.li_getraenkekarte.get(auswahl).name, anzahl, Eigene_Pizza.format(Double.parseDouble((GUI.li_getraenkekarte.get(auswahl).preis)) * anzahl) + "€"});
+		
 		GUI.hinzufuegensound();
 	}
 	/** speisenLaden
